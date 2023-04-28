a4d34b01c07c714add1b1a8c120415349a955d7f
fatih <fatiht@shipmercury.com>
2023-04-28 16:21:32 +0300

    ** typo

--- src/designpatterns/behavioral/chainofresponsibility/OrderChainBuilder.java
+++ src/designpatterns/behavioral/chainofresponsibility/OrderChainBuilder.java
@@ -4,18 +4,18 @@ public class OrderChainBuilder {
 
   public static OrderHandler getOrderChain() {
     OrderHandler createOrderHandler = new CreateOrderHandler("CreateOrderHandler", "Order create error");
-    OrderHandler balanceControlHandler = new BalanceControlHandler("BalanceControlHandler", "not enough balance");
+    OrderHandler balanceControlHandler = new BalanceControlHandler("BalanceControlHandler", "Not enough balance");
     balanceControlHandler.setAnswerIsYesOrderHandler(createOrderHandler);
 
-    OrderHandler licenseDateControlHandler = new LicenseDateControlHandler("LicenseDateControlHandler", "license expired");
+    OrderHandler licenseDateControlHandler = new LicenseDateControlHandler("LicenseDateControlHandler", "License expired");
     licenseDateControlHandler.setAnswerIsNoOrderHandler(balanceControlHandler);
     licenseDateControlHandler.setAnswerIsYesOrderHandler(createOrderHandler);
 
-    OrderHandler ageControlHandler = new AgeControlHandler("AgeControlHandler", "age is not valid");
+    OrderHandler ageControlHandler = new AgeControlHandler("AgeControlHandler", "Age is not valid");
     ageControlHandler.setAnswerIsNoOrderHandler(licenseDateControlHandler);
     ageControlHandler.setAnswerIsYesOrderHandler(balanceControlHandler);
 
-    OrderHandler vipHandler = new VipHandler("VipHandler", "not a vip customer");
+    OrderHandler vipHandler = new VipHandler("VipHandler", "Not a vip customer");
     vipHandler.setAnswerIsNoOrderHandler(ageControlHandler);
     vipHandler.setAnswerIsYesOrderHandler(createOrderHandler);
 
